f573cfc14dbbc78565610033ea5c1fe4c2472668
Samaheh Loghmani <>
2021-12-30 21:03:39 -0500

    created filter search

---
+++ src/components/Coin.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Coin = ({ image, name, symbol, price, marketCap, priceChange }) => {
+    return (
+        <div>
+            <img src={image} alt={name} />
+            <span>{name}</span>
+            <span>{symbol.toUpperCase()}</span>
+            <span>{price.toLocaleString()}</span>
+            <span>{priceChange}</span>
+
+            <span>{marketCap.toLocaleString()}</span>
+        </div>
+    );
+};
+
+export default Coin;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { getCoins } from "../services/api";
+import Coin from "./Coin";
 import Loader from "./Loader";
 
 const Landing = () => {
     const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         const fetchAPI = async () => {
@@ -15,13 +17,34 @@ const Landing = () => {
         fetchAPI();
     }, []);
 
+    const searchHandler = (event) => {
+        setSearch(event.target.value);
+    };
+
+    const searchedCoins = coins.filter((coin) =>
+        coin.name.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <>
-            <input type="text" placeholder="Search" />
+            <input
+                type="text"
+                placeholder="Search"
+                value={search}
+                onChange={searchHandler}
+            />
             {coins.length ? (
                 <div>
-                    {coins.map((coin) => (
-                        <p key={coin.id}>{coin.name}</p>
+                    {searchedCoins.map((coin) => (
+                        <Coin
+                            key={coin.id}
+                            image={coin.image}
+                            name={coin.name}
+                            symbol={coin.symbol}
+                            price={coin.current_price}
+                            marketCap={coin.market_cap}
+                            priceChange={coin.price_change_percentage_24h}
+                        />
                     ))}
                 </div>
             ) : (
